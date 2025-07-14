9bec7ddf8d58a98d68152d017f98212417db8c58
asher.muneer <119673723+asherMuneer97@users.noreply.github.com>
2025-07-14 13:52:33 +0500

    implmenet category relationShip

--- src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,17 +1,24 @@
 import { Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AddBlogpost } from '../models/add-blog-post.model';
 import { BlogPostService } from '../services/blog-post.service';
+import { CategoryService } from '../../category/services/category.service';
+import { Observable } from 'rxjs';
+import { Category } from '../../category/modals/category.model';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
   styleUrls: ['./add-blogpost.component.css']
 })
-export class AddBlogpostComponent {
+export class AddBlogpostComponent implements OnInit {
   model: AddBlogpost;
+  categories$?: Observable<Category[]>;
 
-  constructor(private blogPostService: BlogPostService,private router: Router){
+  constructor(private blogPostService: BlogPostService,
+    private router: Router,
+    private categoryService: CategoryService
+  ){
     this.model = {
       title:'',
       shortDescription: '',
@@ -20,12 +27,17 @@ export class AddBlogpostComponent {
       urlHandle: '',
       author: '',
       isVisible: true,
-      publishedDate: new Date()
+      publishedDate: new Date(),
+      categories: []
     }
+  }
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllcategories();
   }
 
 
   onFormSubmit(): void{
+    console.log(this.model);
     this.blogPostService.createBlogPost(this.model).subscribe({
       next: (response) =>{
         this.router.navigateByUrl('/admin/blogposts');
